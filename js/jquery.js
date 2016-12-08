1fbdb6665b9bff83954f497de0b5956161c91daf
allygator <allygator95@gmail.com>
2016-12-08 13:20:44 -0500

    Footer Day/Night JS

--- js/jquery.js
+++ js/jquery.js
@@ -2,24 +2,24 @@ $('.switch').click(function() {
     if ($('#checkbox').is(":checked")) {
         $("nav").removeClass("day");
         $("nav").addClass("night");
-        $(".navcontainer").removeClass("day");
-        $(".navcontainer").addClass("night");
         $("#resume").removeClass("day");
         $("#resume").addClass("night");
         $("#projects").removeClass("day");
         $("#projects").addClass("night");
         $("#about").removeClass("day");
         $("#about").addClass("night");
+        $("#footer").removeClass("day");
+        $("#footer").addClass("night");
     } else {
         $("nav").removeClass("night");
         $("nav").addClass("day");
-        $(".navcontainer").removeClass("night");
-        $(".navcontainer").addClass("day");
         $("#resume").removeClass("night");
         $("#resume").addClass("day");
         $("#projects").removeClass("night");
         $("#projects").addClass("day");
         $("#about").removeClass("night");
         $("#about").addClass("day");
+        $("#footer").removeClass("night");
+        $("#footer").addClass("day");
     }
 });
