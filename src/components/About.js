91a70f17558991925c5a60db1cb43516b6c562e3
Alyssa <allygator95@gmail.com>
2018-09-03 23:30:55 -0400

    Added fonts, improved mobile layout.

--- src/components/About.js
+++ src/components/About.js
@@ -6,21 +6,21 @@ class About extends Component {
 	return (
 		<div id="about">
 			<h2>About Me</h2>
-			<div id="student">
+			<section id="student">
 				<p>Hi, I&#39;m Alyssa Davis. I am a student at the University of Central Florida studying <span className="emphasis">Computer Science</span> along with minors in <span className="emphasis">Engineering Leadership</span> and <span className="emphasis">Business.</span></p>
-			</div>
-			<div id="dev">
+			</section>
+			<section id="dev">
 				<p>I use <span className="emphasis">Java</span> and <span className="emphasis">C</span> in my classes, <span className="emphasis">PHP</span> (in WordPress) at work and <span className="emphasis">JavaScript</span> at home. Currently I am exploring <span className="emphasis">ReactJS</span>. Next up, I am interested in learning <span className="emphasis">Python</span>.</p>
-			</div>
-			<div id="artist">
-				<p>I&#39;ve been around art my entire life. I have worked in many different art forms including stained glass, lampwork beading, clay sculpting, and bronze casting. I am spending time working in photography and photo editing right now. The pictures shown on this site are all taken by me!</p>
-			</div>
-			<div id="traveler">
+			</section>
+			<section id="artist">
+				<p>I&#39;ve been around art my entire life. I have worked in many different art forms including <span className="emphasis">stained glass</span>, <span className="emphasis">lampwork beading</span>, <span className="emphasis">clay sculpting</span>, and <span className="emphasis">bronze casting</span>. I am spending time working in <span className="emphasis">photography</span> and photo editing right now. The pictures shown on this site are all taken by me!</p>
+			</section>
+			<section id="traveler">
 				<p>I am an avid traveler. I&#39;ve been to 4 countries, 2 continents, every state on both American coastlines and over half of all the states total. <span className="emphasis">Hawaii is my white whale.</span></p>
-			</div>
-			<div id="gamer">
+			</section>
+			<section id="gamer">
 				<p>I have played video games for as long as I can remember, and video games are where my development path started. Most of my personal projects have come out of a personal or community need for something related to a game I play. My first interaction with the dev world was signatures and player profiles in Neopets.</p>
-			</div>
+			</section>
 		</div>
 	);
   }
